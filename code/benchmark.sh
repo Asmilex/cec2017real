4b960329cb174c213d4cfb5ff7c385ffad6a6e6c
Asmilex <andresmm@outlook.com>
2021-05-31 18:01:35 +0200

    Actualizo script

--- code/benchmark.sh
+++ code/benchmark.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 make
 
-./mh_battle_royale 10 2> /dev/null
-./mh_battle_royale 30 2> /dev/null
-./mh_battle_royale 50 2> /dev/null
+rm ./results_MH_battle_royale/*
+
+for i in {1..5}
+do
+    ./mh_battle_royale 10 2> /dev/null
+    ./mh_battle_royale 30 2> /dev/null
+done
 
 python extract.py results_MH_battle_royale/
